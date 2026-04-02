601038a1be90533ca136253264f3f6505f43f9b7
sak0a <laurinfrank03@gmail.com>
2026-04-02 04:13:02 +0200

    refactor: InstallCommand extends ExtensionLifecycleCommand
    Remove duplicated deleteDirectory, copyDirectory, cleanupPath
    methods and inline cache clearing.

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Notur\Console\Commands;
 
-use Illuminate\Console\Command;
 use Notur\Events\ExtensionInstalled;
 use Notur\Events\ExtensionUpdated;
 use Notur\ExtensionManager;
@@ -16,7 +15,7 @@ use Notur\Support\NoturArchive;
 use Notur\Support\RegistryClient;
 use Notur\Support\SignatureVerifier;
 
-class InstallCommand extends Command
+class InstallCommand extends ExtensionLifecycleCommand
 {
     protected $signature = 'notur:install
         {extension : The extension ID (vendor/name) or path to a .notur file}
@@ -253,68 +252,11 @@ class InstallCommand extends Command
         }
 
         // Clear caches
-        $this->call('cache:clear');
-        $this->call('view:clear');
+        $this->clearNoturCaches();
 
         $this->info("Extension '{$extensionId}' v{$manifest->getVersion()} installed and enabled.");
 
         return 0;
     }
 
-    private function copyDirectory(string $source, string $dest): void
-    {
-        if (!is_dir($dest)) {
-            mkdir($dest, 0755, true);
-        }
-
-        $iterator = new \RecursiveIteratorIterator(
-            new \RecursiveDirectoryIterator($source, \FilesystemIterator::SKIP_DOTS),
-            \RecursiveIteratorIterator::SELF_FIRST,
-        );
-
-        foreach ($iterator as $item) {
-            $target = $dest . '/' . $iterator->getSubPathname();
-            if ($item->isDir()) {
-                if (!is_dir($target)) {
-                    mkdir($target, 0755, true);
-                }
-            } else {
-                copy($item->getPathname(), $target);
-            }
-        }
-    }
-
-    private function deleteDirectory(string $dir): void
-    {
-        if (!is_dir($dir)) {
-            return;
-        }
-
-        $iterator = new \RecursiveIteratorIterator(
-            new \RecursiveDirectoryIterator($dir, \FilesystemIterator::SKIP_DOTS),
-            \RecursiveIteratorIterator::CHILD_FIRST,
-        );
-
-        foreach ($iterator as $item) {
-            if ($item->isDir()) {
-                rmdir($item->getPathname());
-            } else {
-                unlink($item->getPathname());
-            }
-        }
-
-        rmdir($dir);
-    }
-
-    private function cleanupPath(string $path): void
-    {
-        if (is_file($path) || is_link($path)) {
-            @unlink($path);
-            return;
-        }
-
-        if (is_dir($path)) {
-            $this->deleteDirectory($path);
-        }
-    }
 }
