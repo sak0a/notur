d06ef48093bf165b30f0a8d0dd6a2b81e764d81e
sak0a <laurinfrank03@gmail.com>
2026-04-02 04:08:28 +0200

    refactor: UninstallCommand uses ManagesFilesystem trait
    Remove duplicated deleteDirectory method, use shared trait instead.

--- src/Console/Commands/UninstallCommand.php
+++ src/Console/Commands/UninstallCommand.php
@@ -6,9 +6,12 @@ namespace Notur\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Schema;
+use Notur\Console\Concerns\ManagesFilesystem;
 
 class UninstallCommand extends Command
 {
+    use ManagesFilesystem;
+
     protected $signature = 'notur:uninstall
         {--confirm : Skip interactive confirmation}';
 
@@ -434,28 +437,4 @@ class UninstallCommand extends Command
         return $result === 0;
     }
 
-    /**
-     * Recursively delete a directory.
-     */
-    private function deleteDirectory(string $dir): void
-    {
-        if (!is_dir($dir)) {
-            return;
-        }
-
-        $iterator = new \RecursiveIteratorIterator(
-            new \RecursiveDirectoryIterator($dir, \FilesystemIterator::SKIP_DOTS),
-            \RecursiveIteratorIterator::CHILD_FIRST,
-        );
-
-        foreach ($iterator as $item) {
-            if ($item->isDir()) {
-                rmdir($item->getPathname());
-            } else {
-                unlink($item->getPathname());
-            }
-        }
-
-        rmdir($dir);
-    }
 }
